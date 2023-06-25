893475bebcf9f3da9bd9bbdc756d4f7ea1c09a50
Marissa Ashner <mashner@live.unc.edu>
2023-06-24 23:24:52 -0400

    fix MVN sigma

--- R/helper_functions_gherm.R
+++ R/helper_functions_gherm.R
@@ -33,7 +33,7 @@ psi_hat_i_hermite_aipw = function(data_row, Y, varNamesRHS, par_vec, cens_name, 
     mu <- cov_dist_params$mu_joint[1] +
       cov_dist_params$Sigma_joint[1,3]*cov_dist_params$Sigma_joint[3,3]^(-1)*
       (data_row[cov_vars] %>% as.numeric() - cov_dist_params$mu_joint[3])
-    sigma <- sqrt(cov_dist_params$Sigma_joint[1,1] +
+    sigma <- sqrt(cov_dist_params$Sigma_joint[1,1] -
       cov_dist_params$Sigma_joint[1,3]^2*cov_dist_params$Sigma_joint[3,3]^(-1))
   }
 
@@ -120,12 +120,12 @@ psi_hat_i_hermite_acc = function(data_row, Y, varNamesRHS, par_vec, cens_name, c
     mu <- cov_dist_params$mu_joint[1] +
       cov_dist_params$Sigma_joint[1,3]*cov_dist_params$Sigma_joint[3,3]^(-1)*
       (data_row[cov_vars] %>% as.numeric() - cov_dist_params$mu_joint[3])
-    sigma <- sqrt(cov_dist_params$Sigma_joint[1,1] +
+    sigma <- sqrt(cov_dist_params$Sigma_joint[1,1] -
       cov_dist_params$Sigma_joint[1,3]^2*cov_dist_params$Sigma_joint[3,3]^(-1))
     mu_c <- function(x, cov_dist_params) {cov_dist_params$mu_joint[2] +
       cov_dist_params$Sigma_joint[2, c(1,3)]%*%cov_dist_params$Sigma_joint[c(1, 3), c(1, 3)]^(-1)%*%
       (x - cov_dist_params$mu_joint[2])}
-    sigma_c <- sqrt(cov_dist_params$mu_joint[2] +
+    sigma_c <- sqrt(cov_dist_params$mu_joint[2] -
       cov_dist_params$Sigma_joint[2, c(1,3)]%*%cov_dist_params$Sigma_joint[c(1, 3), c(1, 3)]^(-1)%*%
       cov_dist_params$Sigma_joint[c(1,3), 2])
   }
@@ -154,7 +154,7 @@ multiroot_func_hermite_acc = function(beta_temp, data,
                                        Y, varNamesRHS, par_vec, cens_name, cov_vars, cens_ind,
                                        m_func, cov_dist_params, sigma2, gh_nodes){
   print(beta_temp)
-  pieces = apply(data, 1, function(temp){
+  pieces = apply(data[1,], 1, function(temp){
     p = c(beta_temp, temp[varNamesRHS]) %>% as.numeric()
     names(p) = c(paste0(par_vec, seq(1:length(beta_temp))), varNamesRHS)
     cc_piece = rep(as.numeric(temp[[cens_ind]]), length(beta_temp))*
